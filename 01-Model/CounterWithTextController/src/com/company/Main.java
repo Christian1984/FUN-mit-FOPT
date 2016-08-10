c15dbcb7fa3bc29228f83eefe2b4422f8ae1003a
Chris <chris.hoffmann84@gmail.com>
2016-08-10 14:28:40 +0200

    Initial Commit
    Initial Commit

--- 01-Model/CounterWithTextController/src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    Counter c = new Counter();
-        TextController tc = new TextController(c);
-    }
-}
